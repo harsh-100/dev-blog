4dcac8b4c6eb46712595aee79f92fe4423a867eb
harsh-100 <26harshagarwal@gmail.com>
2023-01-26 13:11:26 +0530

    ➕  added new dependency to read the blogs and gray-matter

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,19 +1,31 @@
 import { NextApiHandler } from "next";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const handler:NextApiHandler =(req,res)=>{
-
-    const {method} = req
-    switch (method) {
-        case "GET":
-            return res.json({ok:'true'});
-           
-    
-        default:
-            return res.status(404).json({message:'Not accepted'});
-
+const handler: NextApiHandler = (req, res) => {
+  const { method } = req;
+  switch (method) {
+    case "GET": {
+      const data = getPostData();
+      return res.json({ ok: data });
     }
-   
-    
 
-}
+    default:
+      return res.status(404).json({ message: "Not accepted" });
+  }
+};
+
+const getPostData = () => {
+  const folderToReadPath = path.join(process.cwd(), "posts");
+  const fileNames = fs.readdirSync(folderToReadPath);
+
+  fileNames.map((oneFile) => {
+    const filePathToRead = path.join(folderToReadPath, oneFile);
+    const fileData = fs.readFileSync(filePathToRead, { encoding: "utf-8" });
+    console.log(matter(fileData));
+  });
+
+  return fileNames;
+};
 export default handler;
